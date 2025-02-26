3ea6de2e508be2757860ec819b25ffcbc5b48a26
Khushal Kumar <khushalbhatt17@gmail.com>
2025-02-26 13:18:10 +0530

    atoms.js file created

---
+++ src/assets/atoms.js
@@ -0,0 +1,17 @@
+import {atom} from 'recoil';
+
+export const networkAtom = atom({
+
+}); 
+
+export const jobsAtom = atom({
+
+});
+
+export const messagesAtom = atom({
+
+});
+
+export const notificationsAtom = atom({
+
+});
